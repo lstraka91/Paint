bc80cd61f525b04c3db9e9a4bd5c5cf4b410def5
student <student@DESKTOP-DKDBLTK>
2017-10-26 09:44:56 +0200

    test 123

--- src/sk/tsystems/paint/MainFrame.java
+++ src/sk/tsystems/paint/MainFrame.java
@@ -2,9 +2,24 @@ package sk.tsystems.paint;
 
 import java.awt.EventQueue;
 import java.awt.Toolkit;
+
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 public class MainFrame extends JFrame {
+	
+	private void loadImageFile() {
+		JFileChooser chooser = new JFileChooser();
+		FileNameExtensionFilter filter = new FileNameExtensionFilter(
+		        "JPG & GIF Images", "jpg", "gif");
+	    chooser.setFileFilter(filter);
+	    int returnVal = chooser.showOpenDialog(this);
+	    if(returnVal == JFileChooser.APPROVE_OPTION) {
+	       System.out.println("You chose to open this file: " +
+	            chooser.getSelectedFile().getName());
+	    }
+	}
 
 	public MainFrame() {
 		super("Paint");
--- src/sk/tsystems/paint/MainPanel.java
+++ src/sk/tsystems/paint/MainPanel.java
@@ -1,7 +1,23 @@
 package sk.tsystems.paint;
 
+import javax.swing.JFileChooser;
 import javax.swing.JPanel;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 public class MainPanel extends JPanel{
 
+	private void openFile() {
+		JFileChooser chooser = new JFileChooser();
+		FileNameExtensionFilter filter = new FileNameExtensionFilter(
+		        "JPG & GIF Images", "jpg", "gif");
+	    chooser.setFileFilter(filter);
+	    int returnVal = chooser.showOpenDialog(this);
+	    if(returnVal == JFileChooser.APPROVE_OPTION) {
+	       System.out.println("You chose to open this file: " +
+	            chooser.getSelectedFile().getName());
+	    }
+	}
+	
+	
+	
 }
