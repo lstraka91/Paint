ca4217f7856a456e1afbc223eef3b1d7c2ba6246
student <student@DESKTOP-DKDBLTK>
2017-10-26 09:57:58 +0200

    add Menu - File

--- src/sk/tsystems/paint/MainFrame.java
+++ src/sk/tsystems/paint/MainFrame.java
@@ -1,13 +1,36 @@
 package sk.tsystems.paint;
 
+import java.awt.BorderLayout;
 import java.awt.EventQueue;
 import java.awt.Toolkit;
 
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
 public class MainFrame extends JFrame {
+	
+	private JPanel contentPane;
+	private JPanel navigation_panel;
+	private JMenuItem menuSave;
+	private JMenuItem menuLoad;
+	private JMenuItem menuExit;
+	
+	public MainFrame() {
+		super("Paint");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		// setUndecorated(true);.
+		setSize(Toolkit.getDefaultToolkit().getScreenSize());
+		getContentPane().add(new MainPanel());
+
+		initPanel();
+	}
 	
 	private void loadImageFile() {
 		JFileChooser chooser = new JFileChooser();
@@ -20,14 +43,38 @@ public class MainFrame extends JFrame {
 	            chooser.getSelectedFile().getName());
 	    }
 	}
-
-	public MainFrame() {
-		super("Paint");
+	
+	private void initPanel() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 950, 500);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
 
-		// setUndecorated(true);.
-		setSize(Toolkit.getDefaultToolkit().getScreenSize());
-		 getContentPane().add(new MainPanel());
+		navigation_panel = new JPanel();
+		contentPane.add(navigation_panel, BorderLayout.NORTH);
+		navigation_panel.setLayout(new BorderLayout(0, 0));
+
+		JPanel menu_panel = new JPanel();
+		navigation_panel.add(menu_panel, BorderLayout.CENTER);
+		menu_panel.setLayout(new BorderLayout(0, 0));
+
+		JMenuBar menuBar = new JMenuBar();
+		menu_panel.add(menuBar, BorderLayout.WEST);
+
+		JMenu mnFiles = new JMenu("File");
+		menuBar.add(mnFiles);
+
+		menuSave = new JMenuItem("Save");
+		mnFiles.add(menuSave);
+
+		menuLoad = new JMenuItem("Load");
+		mnFiles.add(menuLoad);
+
+		menuExit = new JMenuItem("Exit");
+		mnFiles.add(menuExit);
+
 	}
 
 	public static void main(String[] args) {
